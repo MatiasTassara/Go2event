c6494501f6c4722bee7e16b39d206b9886fcc602
MaruCagnoli <37384046+MaruCagnoli@users.noreply.github.com>
2018-11-11 22:52:33 -0300

    Add files via upload

--- model/ticket.php
+++ model/ticket.php
@@ -1,17 +1,16 @@
 <?php
 namespace Model;
-
 class Ticket{
   private $id;
   private $number;
   private $qr;
-
-  public function __construct($number,$qr, $id = ''){
+  private $purchaseItem;
+  public function __construct($number,$qr, $purchItem, $id = ''){
     $this->id = $id;
     $this->number = $number;
     $this->qr = $qr;
+    $this->purchaseItem = $purchItem;
   }
-
   public function getId(){
     return $this->id;
   }
@@ -20,6 +19,9 @@ class Ticket{
   }
   public function getQr(){
     return $this->qr;
+  }
+  public function getPurchaseItem(){
+    return $this->purchaseItem;
   }
   public function setId($id){
     $this->id = $id;
@@ -30,5 +32,4 @@ class Ticket{
   public function setQr($qr){
     $this->qr = $qr;
   }
-
 }
