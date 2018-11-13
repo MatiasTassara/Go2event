203928bb7ed43c3a25ccff3f5ed0fe09d799c606
Licanueto <licanueto@hotmail.com>
2018-11-13 09:57:15 -0300

    Nombres de constantes corregidos

--- config/constants.php
+++ config/constants.php
@@ -4,7 +4,7 @@ define('ROOT', str_replace('\\','/',dirname(__DIR__) . "/"));
 define('FRONT_ROOT', 'http://localhost/Go2Event/');
 
 $base=explode($_SERVER['DOCUMENT_ROOT'],ROOT);
-  define("DB_NAME",$base[1]);
+  define("DB_NAME","go2event_DB");
   define('DB_HOST',"localhost");
   define('DB_USER',"root");
   define('DB_PASS',"");
