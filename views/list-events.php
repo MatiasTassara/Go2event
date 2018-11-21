fc7ef78325830f4f60cfa03ca8445dda182b03ed
iMoyis <37676075+iMoyis@users.noreply.github.com>
2018-11-21 12:37:04 -0300

    List Events Actualizado para busqueda

--- views/list-events.php
+++ views/list-events.php
@@ -1,5 +1,8 @@
 <?php include(ROOT."views/navbar.php"); ?>
+
 <div class="container mb-5">
+<?php if (isset($events) && !empty($events)) { ?>
+
  <h2 class="uk-heading-line uk-text-center"><span>Próximos</span></h2>
   <div class="card-columns">
     <?php foreach ($events as $key => $value) {?>
@@ -14,6 +17,61 @@
     </a>
     <?php  } ?>
   </div>
+  <?php  } ?>
+
+  <?php if (isset($eventsByName) && !empty($eventsByName)) { ?>
+
+   <h2 class="uk-heading-line uk-text-center"><span>Resultados para <?= $text ?></span></h2>
+    <div class="card-columns">
+      <?php foreach ($eventsByName as $key => $value) {?>
+      <a href="<?=FRONT_ROOT?>Home/EventInfo/<?= $value->getId(); ?>">
+      <div class="card">
+        <img class="card-img-top" src="<?= FRONT_ROOT ?>images/<?php echo $value->getImgPath() ?>" alt="Card image cap">
+        <div class="card-body">
+          <h3 class="uk-margin-remove "><?= $value->getName(); ?></h3>
+          <p class="uk-margin-remove uk-text-truncate uk-text-muted"> <?= $value->getDesc()?></p>
+          </div>
+      </div>
+      </a>
+      <?php  } ?>
+    </div>
+    <?php  } ?>
+
+    <?php if (isset($eventsByArtist) && !empty($eventsByArtist)) { ?>
+
+     <h2 class="uk-heading-line uk-text-center"><span>Eventos de <?= $text ?></span></h2>
+      <div class="card-columns">
+        <?php foreach ($eventsByArtist as $key => $value) {?>
+        <a href="<?=FRONT_ROOT?>Home/EventInfo/<?= $value->getId(); ?>">
+        <div class="card">
+          <img class="card-img-top" src="<?= FRONT_ROOT ?>images/<?php echo $value->getImgPath() ?>" alt="Card image cap">
+          <div class="card-body">
+            <h3 class="uk-margin-remove "><?= $value->getName(); ?></h3>
+            <p class="uk-margin-remove uk-text-truncate uk-text-muted"> <?= $value->getDesc()?></p>
+            </div>
+        </div>
+        </a>
+        <?php  } ?>
+      </div>
+      <?php  } ?>
+
+      <?php if (isset($eventsByCategory) && !empty($eventsByCategory)) { ?>
+
+       <h2 class="uk-heading-line uk-text-center"><span>Categorias</span></h2>
+        <div class="card-columns">
+          <?php foreach ($eventsByCategory as $key => $value) {?>
+          <a href="<?=FRONT_ROOT?>Home/EventInfo/<?= $value->getId(); ?>">
+          <div class="card">
+            <img class="card-img-top" src="<?= FRONT_ROOT ?>images/<?php echo $value->getImgPath() ?>" alt="Card image cap">
+            <div class="card-body">
+              <h3 class="uk-margin-remove "><?= $value->getName(); ?></h3>
+              <p class="uk-margin-remove uk-text-truncate uk-text-muted"> <?= $value->getDesc()?></p>
+              </div>
+          </div>
+          </a>
+          <?php  } ?>
+        </div>
+        <?php  } ?>
 
   </div>
 
