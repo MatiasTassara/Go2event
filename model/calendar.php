ce131175232ee45daaf7e1bb25d9868147a4b10b
iMoyis <37676075+iMoyis@users.noreply.github.com>
2018-11-21 23:11:40 -0300

    Metodo getDate( ) actualizado

--- model/calendar.php
+++ model/calendar.php
@@ -3,12 +3,14 @@ namespace model;
 
 class Calendar{
   private $id;
+  //private $artists; // clase artista array
   private $venue;
   private $date;
   private $event;
 
-  function __construct( $venue, $event, $date,$id=''){
+  function __construct(/*$artists,*/ $venue, $event, $date,$id=''){
     $this->id = $id;
+   // $artists = $artists;
     $this->venue = $venue;
     $this->event = $event;
     $this->date = $date;
@@ -18,18 +20,27 @@ class Calendar{
   public function getId(){
     return $this->id;
   }
-
+/*
+  public function getArtists(){
+    return $this->$artists;
+  }
+*/
   public function getVenue(){
     return $this->venue;
   }
 
   public function getDate(){
-    return $this->date;
+    $date = date("d/m/Y",strtotime($this->date));
+    return $date;
   }
   public function getEvent(){
     return $this->event;
   }
 
+/*
+  public function setArtists($artists){
+    $this->artists = $artists;
+  }*/
   public function setVenue($venue){
     $this->venue = $venue;
   }
