ac321d837a982413294c3f93e4b80be856b7c01b
MaruCagnoli <37384046+MaruCagnoli@users.noreply.github.com>
2018-11-25 13:37:53 -0300

    Add files via upload

--- CREATE DATABASE Go2event_DB.sql
+++ CREATE DATABASE Go2event_DB.sql
@@ -5,6 +5,7 @@ CREATE TABLE categories(
 
 	id_category int unsigned auto_increment,
 	name_category varchar(50),
+	active int unsigned,
 	constraint pk_categories primary key (id_category)
 	
 );
@@ -16,6 +17,7 @@ CREATE TABLE events(
 	description varchar(300),
 	img_path varchar(200),
 	id_category int unsigned,
+	active int unsigned,
 	constraint pk_events primary key (id_event),
 	constraint fk_events_categories foreign key (id_category) references categories (id_category)
 );
@@ -27,6 +29,7 @@ CREATE TABLE venues(
 	address varchar(50),
 	city varchar(50),
 	capacityLimit int unsigned,
+	active int unsigned,
 	constraint pk_venues primary key (id_venue)
 	
 );
@@ -36,6 +39,7 @@ CREATE TABLE seattypes(
 	id_seattype int unsigned auto_increment,
 	name_seattype varchar(50),
 	description varchar (80),
+	active int unsigned,
 	constraint pk_seattypes primary key (id_seattype)
 	
 );
@@ -44,6 +48,7 @@ CREATE TABLE artists(
 	id_artist int unsigned auto_increment,
 	name_artist varchar(50),
 	description varchar (80),
+	active int unsigned,
 	constraint pk_id_artist primary key (id_artist)
 	
 );
@@ -55,6 +60,7 @@ CREATE TABLE calendars(
 	img_path varchar(200),
 	id_venue int unsigned,
 	id_event int unsigned,
+	active int unsigned,
 	constraint pk_calendars primary key (id_calendar),
 	constraint fk_calendars_venues foreign key (id_venue) references venues (id_venue),
 	constraint fk_calendars_events foreign key (id_event) references events (id_event)
@@ -77,6 +83,7 @@ CREATE TABLE seats(
 	remaining int unsigned,
 	id_seattype int unsigned,
 	id_calendar int unsigned,
+	active int unsigned,
 	constraint pk_seats primary key (id_seat),
 	constraint fk_seats_calendars foreign key (id_calendar) references calendars (id_calendar),
 	constraint fk_seats_seattypes foreign key (id_seattype) references seattypes (id_seattype)
